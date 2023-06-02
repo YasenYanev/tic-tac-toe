6cbdd6f0c19a140922fa66286c3bd97b1b206141
Yasen Yanev <124697579+YasenYanev@users.noreply.github.com>
2023-06-02 21:36:04 +0300

    Progress in the gameboard area.
    Added the ability to put two different tiles on the gameboard and included tracking of the active player below the gameboard.

--- index.js
+++ index.js
@@ -2,45 +2,65 @@ const cells = document.querySelectorAll("[data-cell]")
 const marks = document.querySelectorAll("[data-mark]")
 const gameBoard = document.getElementById("gameBoard")
 
-gameBoardCells = [
-    null, null, null, 
-    null, null, null,  
-    null, null, null]
 
 const gameBoardState = (() => {
-    let activePlayer = ""
-    const setActivePlayer = (mark) => {
-        activePlayer = mark 
-    }
-    const updateDisplay = () => {
-        
+    let gameBoardCells = [
+        "cell1", "cell2", "cell3", 
+        "cell4", "cell5", "cell6",  
+        "cell7", "cell8", "cell9"]
+
+    let activePlayer = "X"
+
+    cells.forEach(cell => {
+        let cellNumber
+        cell.addEventListener("click", (e) => {
+            cellNumber = e.originalTarget.id
+            cellNumToArrayNum = cellNumber - 1
+            if (gameBoardCells.indexOf("X") && gameBoardCells.indexOf("O") === cellNumToArrayNum) {
+                return
+            }
+
+            if (activePlayer === "X") {
+                cell.children[0].innerHTML = activePlayer
+                gameBoardCells.splice(cellNumToArrayNum, 1, "X")
+                updateActivePlrDisplay(".plr2", ".plr1")
+                activePlayer = "O"
+
+            } else if (activePlayer === "O") {
+                cell.children[0].innerHTML = activePlayer
+                gameBoardCells.splice(cellNumToArrayNum, 1, "O")
+                updateActivePlrDisplay(".plr1", ".plr2")
+                activePlayer = "X"
+
+            }
+        })
+    })
+
+    const updateActivePlrDisplay = (player, player2) => {
+        document.querySelector(player).style.background = "white"
+        document.querySelector(player).style.color = "black"
+
+        document.querySelector(player2).style.background = "black"
+        document.querySelector(player2).style.color = "white"
     }
     const resetGame = () => {
         marks.forEach(marker => {
             marker.innerHTML = ""
         })
     }
-    const startGame = () => {
-        activePlayer = "X"
-    }
-
-    startGame()
-    return {setActivePlayer, activePlayer}
+    return { }
 })()
 
+const PlayerFactory = (name) => {
+    let wins = undefined
 
-const PlayerFactory = () => {
-
+    return {name}
 }
 
-cells.forEach(cell => {
-    cell.addEventListener("click", () => {
-        cell.children[0].innerHTML = gameBoardState.activePlayer
-    })
-})
 
 
-// const Player1 = PlayerFactory(X)
-// const player2 = PlayerFactory()
+
+const player1 = PlayerFactory("X")
+const player2 = PlayerFactory("O")
 
 
